602c9c0e18a2bf0a809d673754c6731cbcfa8f0f
Akkana Peck <akkana@shallowsky.com>
2012-02-19 12:24:30 -0800

    Add the master Makefile at the top level, plus a basic blink sketch

---
+++ blink/blink.ino
@@ -0,0 +1,24 @@
+/* -*- c -*- */
+
+/*
+ * Arduino basic blink
+ */
+
+/* Times in milliseconds */
+#define DELAYTIME   500
+
+int ledPin = 13;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(ledPin, LOW);
+    delay(DELAYTIME*2);
+}
+
