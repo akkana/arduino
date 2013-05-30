c491cf7e5d7bd78d8958cc2cec1efaba19f414a5
Akkana Peck <akkana@shallowsky.com>
2013-05-29 21:39:23 -0700

    Check in some of the musicshield code

--- musicshield/play-repeat/play-repeat.ino
+++ musicshield/play-repeat/play-repeat.ino
@@ -1,3 +1,5 @@
+#include "debug.h"
+
 #include <avr/io.h>
 #include "config.h"
 #include "filesys.h"
@@ -5,25 +7,14 @@
 #include "record.h"
 #include "player.h"
 
-// the music library doesn't bother to export these:
-extern void PlayCurrentFile();
-extern void ControlLed();
-extern unsigned char g_volume;
-extern int redPwm;             // brightness of red led
-extern unsigned int greenFreq; // flash frequency of green led
-
-void PlayIt() {
-    // Start the current file playing, maybe a repeat.
-    Serial.print("Calling PlayCurrentFile ");
-    Serial.println(currentFile);
-    PlayCurrentFile();
-    Serial.println("Returned from PlayCurrentFile()");
-}
+#define DEBUG 1
 
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Test Player");
+#ifdef DEBUG
+    PRINTln("Test Player");
+#endif
   
     InitSPI();
     InitIOForVs10xx();
@@ -31,117 +22,175 @@ void setup()
     InitIOForLEDs();
     InitFileSystem();
 
+    // issues:
+    // tracks 4 and 5 are too soft, so are 8 and 9, 11, 12, 13, 15
+    // at 16, loses place, doesn't wrap around. But another push
+    // and it works.
+
     //VsSineTest();
 
     Mp3Reset();
+    analogWrite(9, 10);    // make the red LED (pin 9) really dim
+    GREEN_LED_OFF();       // no brightness adjustment on the green LED
 
     currentFile = 1;
+    int lastFile = -1;
     while (1) {
-        Mp3SoftReset();
-        Serial.print("setup loop: currentFile = ");
+        int rv;
+
+        // Are we playing a new file? Then we need to reset the MP3,
+        // otherwise it'll get confused and stop playing anything.
+        // It would be nice to do that in CheckButtons where the
+        // file is actually changed, but for some reason Mp3SoftReset()
+        // doesn't work from there, only from here.
+        // All I can guess is that this must have something to do with
+        // things the player code is doing after passing control back from
+        // AvailableProcessorTime().
+        if (currentFile != lastFile)
+            Mp3SoftReset();
+        lastFile = currentFile;
+
+#ifdef DEBUG
+        PRINT("setup loop: currentFile = ");
         Serial.println(currentFile);
-        while (OpenFile(currentFile)) {
-            Serial.print("Oh no! Couldn't open file ");
+#endif
+        while ((rv = OpenFile(currentFile))) {
+            if (rv == 0x0c) // "File not found" which actually means end of dir
+            {
+#ifdef DEBUG
+                PRINT("End of directory -- cycling back from ");
+                Serial.println(currentFile);
+                PRINTln("=========================");
+#endif
+                if (currentFile == 0) {
+#ifdef DEBUG
+                    PRINTln("Can't read any files -- bailing");
+#endif
+                    delay(500000);
+                }
+                // I'm not sure if this is actually needed:
+                //unsigned int initFS = (unsigned int)(InitFileSystem());
+#ifdef DEBUG_NOT
+                PRINT("================\nInitFileSystem() returned ");
+                Serial.println(initFS, HEX);
+#endif
+                currentFile = 1;
+                continue;
+            }
+
+#ifdef DEBUG
+            PRINT("Oh no! Couldn't open file ");
             Serial.println(currentFile);
+#endif
             ++currentFile;
         }
 
-        Serial.println("Playing it!");
-        PlayIt();
+#ifdef DEBUG
+        PRINT("Setting volume to ");
+        Serial.println(g_volume);
+        Mp3SetVolume(g_volume, g_volume);
+        PRINT("Playing file ");
+        Serial.println(currentFile);
+#endif
+        PlayCurrentFile();
     }
 }
 
-// When loop is called, the file should already have been opened.
-// But actually, loop is never called since playing consumes the CPU forever.
+// The normal Arduino loop() is never called
+// since playing consumes the CPU forever.
 // If it does get called, we're confused and not doing anything useful,
 // so delay a bit.
 void loop() {
-    Serial.print("Loop");
+#ifdef DEBUG
+    PRINT("Loop");
+#endif
     delay(500);
 }
 
-// This almost works! Need to do button debouncing -- pressing the button
-// skips forward many tracks, not just one -- and add the volume buttons
-// back in.
-void AvailableProcessorTime()
+void CheckButtons()
 {
-    if (0 == PSKey)    // main button was pressed -- switch to the next song.
+    static unsigned int lastButtonPress = 0;
+
+    // How long has it been? Don't check buttons if one was pressed recently.
+    int curTime = millis();
+    if (curTime - lastButtonPress < 200)
+        return;
+
+    if (0 == PSKey || 0 == NTKey || 0 == BKKey)
     {
-        Serial.println("\nButton pressed!");
+#ifdef DEBUG
+        PRINTln("\nButton pressed!");
+#endif
+        lastButtonPress = curTime;
 
         // Try to signal the player that we want to stop
         playingState = PS_END_OF_SONG;
-        Serial.print("Incrementing currentFile to ");
+#ifdef DEBUG
+        PRINT("Incrementing currentFile to ");
         Serial.println(currentFile);
-        ++currentFile;
+#endif
+        if (0 == BKKey)
+            --currentFile;
+        else
+            ++currentFile;
         delay(500);       // button debounce, don't advance more than 1 song
         // I hate to do a debounce delay in AvailableProcessorTime(),
         // but it doesn't work if we wait until we get back to setup() --
         // we'll get several more button presses first.
+
+        // For some reason, Mp3SoftReset() here doesn't work.
+        // It only works if it's called from the main loop --
+        // right after we return from this function!
+        //Serial.println("Soft resetting");
+        //Mp3SoftReset();
+    }
+
+    // The combo button can also go forward or back.
+    // To do something with it, use these:
+    else if (0 == NTKey) {
+#ifdef DEBUG
+        PRINTln("\nNext song button");
+#endif
+        lastButtonPress = curTime;
+    }
+    else if (0 == BKKey) {
+#ifdef DEBUG
+        PRINTln("\nPrevious song button");
+#endif
+        lastButtonPress = curTime;
     }
 
     // Volume controls. Mp3SetVolume(leftchannel, rightchannel)
     // apparently takes values between 0 and 0xff.
     // Code apparently increases g_volume for vd, decreases for vu.
     // Go figure!
-    else if (0 == VUKey)    // volume up
+    // Also: the volume increment here is quite small.
+    // But because there's no button debouncing, in practice
+    // one button press creates quite a large volume difference.
+    // Would probably be good to check millis().
+    else if (0 == VUKey || 0 == VDKey)    // volume up or down
     {
-        Serial.println("VUKey");
-        static unsigned int vu_cnt = 1000;  //volume up interval
-  	if (--vu_cnt == 0)
-	{
-            if (g_volume-- == 0)
-                g_volume = 0; //Change + limit to 0 (maximum volume)
-    
-            Mp3SetVolume(g_volume, g_volume);
+        lastButtonPress = curTime;
+        if (0 == VUKey && g_volume-- < 0)
+            g_volume = 0;     // limit to 0 (maximum volume)
+        else if (0 == VDKey && g_volume++ == 254)
+            g_volume = 254;   // limit to 254 (minimum vol)
 
-#if 0
-            // Original code set the redPwm here,
-            // but I can't see a visible difference
-
-            redPwm = (175-g_volume)*3>>1;
-            if (redPwm >255)
-            {
-                redPwm = 255;
-            }
-            if (redPwm < 0)
-            {
-                redPwm = 0;
-            }
-            //Serial.println(redPwm,DEC);
+        Mp3SetVolume(g_volume, g_volume);
+#ifdef DEBUG
+        PRINT("Setting volume to ");
+        Serial.println(g_volume);
 #endif
-            vu_cnt = 1000;
-            // debounce
-            delay(200);
-        }
     }
-    else if (0 == VDKey)
-    {
-        Serial.println("VDKey");
-        static unsigned int vd_cnt = 1000;  // volume down interval
-        if (--vd_cnt == 0)
-	{
-            if (g_volume++ == 254)
-                g_volume = 254; //Change + limit to 254 (minimum vol)
-	
-            Mp3SetVolume(g_volume,g_volume);
+}
 
-            redPwm = 305-(g_volume<<1);
-            if (redPwm >255)
-            {
-                redPwm = 255;
-            }
-            if (redPwm < 0)
-            {
-                redPwm = 0;
-            }
-            //Serial.println(redPwm,DEC);
-            vd_cnt = 1000;
-        }
-    }
+void AvailableProcessorTime()
+{
+    // See if any buttons are pressed, and react accordingly
+    CheckButtons();
 
-    // Do mysterious things related to the LEDs.
-    ControlLed();
+    // Do mysterious and non visually obvious things related to the LEDs.
+    //ControlLed();
 }
 
 
