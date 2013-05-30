d67a4f3a7367a9841ada12031c66d1783820e1e0
Akkana Peck <akkana@shallowsky.com>
2013-05-30 09:58:01 -0700

    Code cleanup, and add some extra explanatory comments

--- music/theremin/theremin.ino
+++ music/theremin/theremin.ino
@@ -3,16 +3,35 @@
  */
 
 // what digital pin is the speaker on? (other speaker wire is Gnd.)
-uint8_t SPEAKER = 10;
+int SPEAKER = 10;
 
 // which analog pin is the light sensor on?
-uint8_t LIGHTSENSOR = 0;
+int LIGHTSENSOR = 0;
+
+// Typical photocell values for daytime with window and 1k resistor:
+// min 15, max 100.
+#define MIN_SIGNAL   15  /* 100 */
+#define MAX_SIGNAL  100  /* 1024 */
+
+// What frequencies should we allow (in Hertz)?
+// Typical human hearing is 20 Hz to 20,000 Hz,
+// but our piezo buzzers can't reproduce such a wide range.
+// 100 to 10k or so is usually okay.
+#define MIN_FREQ    100
+#define MAX_FREQ   8000
+
+// How many samples should we read from the sensor each time?
+// The sensor is noisy, so there will be a lot of spurious readings.
+#define NUM_SAMPLES  15
+
+// To monitor the output from the sensor (so you can calibrate MAX_SIGNAL
+// and MIN_SIGNAL), uncomment SERIAL_DEBUG:
+//#define SERIAL_DEBUG 1
 
 void setup()
 {
     pinMode(SPEAKER, OUTPUT);
 
-//#define SERIAL_DEBUG 1
 #ifdef SERIAL_DEBUG
     Serial.begin(9600);
 #endif
@@ -21,17 +40,13 @@ void setup()
 void loop()
 {
     // Set the frequency according to the light value read off analog pin 0.
-    // Typical night/indoor Light sensor values:
-    // 110-290 (low light), 490 (room light), 980 (direct flashlight).
-    // Typical for daytime with window:
-    // 325 (hand over sensor), 790 (sunlight through window)
-    // Want frequency between 100 and 10000 (Hz).
-#define MIN_FREQ    100
-#define MAX_FREQ   6000
-#define MIN_SIGNAL  100  /* 100 */
-#define MAX_SIGNAL  512  /* 1024 */
 
-    int lightsensor = analogRead(LIGHTSENSOR);
+    // Average 5 reads
+    int lightsensor = 0;
+    for (int i=1; i < NUM_SAMPLES; ++i)
+        lightsensor += analogRead(LIGHTSENSOR);
+    lightsensor /= NUM_SAMPLES;
+
     unsigned int freq = map(lightsensor,
                             MIN_SIGNAL, MAX_SIGNAL,
                             MIN_FREQ, MAX_FREQ);
@@ -43,7 +58,7 @@ void loop()
     Serial.print(lightsensor);
     Serial.print(" -> ");
     Serial.println(freq);
-    delay(200);
+    delay(500);
 #endif
 }
 
