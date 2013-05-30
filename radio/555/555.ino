454daf57325e5ef8ee5fcd2d187bd5dbccd12f06
Akkana Peck <akkana@shallowsky.com>
2013-05-29 21:27:30 -0700

    Use the FreqCounter library. Or try -- this probably doesn't work, and is purely experimental.

--- radio/555/555.ino
+++ radio/555/555.ino
@@ -1,27 +1,43 @@
-/* -*- c -*- */
-/*
- * Announce when we see anything on an incoming pin.
- * The idea is to watch the radio receiver pin and see when it's transmitting.
- * (Then use a 555 to drive the transmitter.)
- * Unfortunately these radios don't work like that and can only send
- * very short logic pulses, not pulses long enough to see.
- */
+//
+// Detect whether our radio signal is present, using the FreqCounter library.
+// Based on the Frequency Counter Lib example.
+// should give 6 or 7 when signal is present, 0 otherwise.
 
-void setup()
-{
-    pinMode(13, OUTPUT);
+// Note: another way to do it might use pulsein,
+// http://www.arduino.cc/en/Reference/PulseIn
 
-    Serial.begin(9600);	// Debugging only
-    Serial.println("Radio receiver");
+#include <FreqCounter.h>
+
+unsigned long frq;
+int cnt;
+int pinLED=13;
+
+void setup() {
+  pinMode(pinLED, OUTPUT);
+
+  Serial.begin(115200);        // connect to the serial port
+
+  Serial.println("Frequency Counter");
 }
 
-void loop()
-{
-    int on = analogRead(1);
-    digitalWrite(13, on);
+void loop() {
 
-    Serial.println(on);
+  // wait if any serial is going on
+  FreqCounter::f_comp=10;   // Cal Value / Calibrate with professional Freq Counter
+  FreqCounter::start(100);  // 100 ms Gate Time
 
-    delay(100);
-}
+  while (FreqCounter::f_ready == 0) 
+
+  frq=FreqCounter::f_freq;
+  Serial.print(cnt++);
+  Serial.print("  Freq: ");
+  Serial.println(frq);
+  if (frq == 6 || frq == 7)
+      digitalWrite(pinLED, 0);
+  else
+      digitalWrite(pinLED, 1);
+  delay(20);
+  //digitalWrite(pinLed,!digitalRead(pinLED));  // blink Led
+
+}  
 
