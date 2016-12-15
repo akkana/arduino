bc29a9dd43b44aff3e23922b5a173415e8d91353
Akkana Peck <akkana@shallowsky.com>
2016-12-14 17:52:01 -0700

    Make a 'too high' a variable instead of an inline constant

--- music/rising/rising.ino
+++ music/rising/rising.ino
@@ -1,20 +1,21 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 /* A rising tone, repeated forever */
 
-// what digital pin is the speaker on? (other speaker wire is Gnd.)
-uint8_t SPEAKER = 8;
+int SPEAKER = 8;           // What digital pin is the speaker on?
+
+int freq = 50;             // Starting frequency
+
+int TOOHIGH = 6000;
 
 void setup()
 {
     pinMode(SPEAKER, OUTPUT);
 }
 
-unsigned int freq = 20;
-
 void loop()
 {
-    /* Reset the frequency if we go too high */
-    if (freq > 20000) {
+    // Reset the frequency if we go too high
+    if (freq > TOOHIGH) {
+        noTone(SPEAKER);
         delay(2000);
         freq = 20;
     }
@@ -22,5 +23,8 @@ void loop()
     tone(SPEAKER, freq);
 
     freq = (int)(freq * 1.05);
+    //freq += 100;
+
+    delay(100);
 }
 
