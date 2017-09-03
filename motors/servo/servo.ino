a77a30b8970ba28aec7d51d8df05a3ae1f631b48
Akkana Peck <akkana@shallowsky.com>
2017-09-03 10:31:46 -0600

    Better tests

--- motors/servo/servo.ino
+++ motors/servo/servo.ino
@@ -1,40 +1,54 @@
-/* -*- c -*- */
-#line 3
 
 #include <Servo.h>
- 
+
+#define TWO
+
 Servo myservo;  // create servo object to control a servo
                 // a maximum of eight servo objects can be created
- 
+#ifdef TWO
+Servo myservo2;
+#endif
+
 int pos = 0;    // variable to store the servo position
- 
+
 void setup()
 {
     myservo.attach(7);
+    myservo.write(0);
+#ifdef TWO
+    myservo2.attach(8);
+    myservo2.write(0);
+#endif
+    delay(3000);
 
+    /*
     delay(1000);
     myservo.write(10);
     delay(1000);
     myservo.write(170);
     delay(1000);
     myservo.write(90);
+    */
 }
- 
- 
+
+
 void loop()
 {
+    for (pos = 0; pos <= 180; pos += 1) {
+        myservo.write(pos);
+#ifdef TWO
+        myservo2.write(pos);
+#endif
+        delay(15);
+    }
     delay(1000);
-    return;
 
-    for (pos = 0; pos < 180; pos += 1)
-    {
+    for (pos = 180; pos >= 0; pos -= 1) {
         myservo.write(pos);
+#ifdef TWO
+        myservo2.write(pos);
+#endif
         delay(15);
     }
-    for (pos = 180; pos>=1; pos-=1)
-    {                                
-        myservo.write(pos);
-        delay(15);
-    }
+    delay(3000);
 }
-
