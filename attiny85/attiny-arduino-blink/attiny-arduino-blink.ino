9f88a2e2387d1539e15f82998f762dfeacb8ad55
Akkana Peck <akkana@shallowsky.com>
2017-11-02 13:52:46 -0600

    Add a second LED

--- attiny85/attiny-arduino-blink/attiny-arduino-blink.ino
+++ attiny85/attiny-arduino-blink/attiny-arduino-blink.ino
@@ -6,17 +6,21 @@
 #define DELAYTIME   500
 
 int ledPin = 3;
+int ledPin2 = 2;
 
 void setup()
 {
     pinMode(ledPin, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
 }
 
 void loop()
 {
     digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin2, LOW);
     delay(DELAYTIME);
     digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, HIGH);
     delay(DELAYTIME);
 }
 
