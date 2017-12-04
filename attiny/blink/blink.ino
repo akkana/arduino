3dac9a33bb160453ba08af7fd535c12691a2369e
Akkana Peck <akkana@shallowsky.com>
2017-12-03 18:40:27 -0700

    Add attiny blink example

---
+++ attiny/blink/blink.ino
@@ -0,0 +1,25 @@
+/* -*- c -*- */
+#line 3
+
+/*
+ * Arduino basic blink
+ */
+
+/* Times in milliseconds */
+#define DELAYTIME   500
+
+int ledPin = 13;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(ledPin, HIGH);
+    delay(DELAYTIME);
+    digitalWrite(ledPin, LOW);
+    delay(DELAYTIME);
+}
+
